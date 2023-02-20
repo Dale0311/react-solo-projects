b9f8d1ee7bebda9fea69d7f31a8f273e1b015bf6
Dale <Danntabachoyy@gmail.com>
2023-02-21 06:51:10 +0800

    added some few data

--- project2/src/components/data/dataTravel.jsx
+++ project2/src/components/data/dataTravel.jsx
@@ -28,5 +28,35 @@ export default [
         endDate: "24 Jan, 2021",
         description: "Iconic fjord offering snowy mountain peaks, lush plant life, cascading falls & spectacular views.",
         imageUrl: "img3.jpg"
+    },
+    {
+        id: 4,
+        title: "Boracay",
+        location: "Philippines",
+        googleMapsUrl: "https://goo.gl/maps/1wjhDxEdJqE74KQK8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Small island known for its resorts, nightlife & striking, white-sand beaches.",
+        imageUrl: "img4.jpg"
+    },
+    {
+        id: 5,
+        title: "Eifel Tower",
+        location: "France",
+        googleMapsUrl: "https://goo.gl/maps/jVrEbYeHuzuHD5dV9",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Gustave Eiffel's iconic, wrought-iron 1889 tower, with steps and elevators to observation decks.",
+        imageUrl: "img5.jpg"
+    },
+    {
+        id: 6,
+        title: "Mont-Saint-Michel",
+        location: "France",
+        googleMapsUrl: "https://goo.gl/maps/wsNYgCAyyJccX2Ek6",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mont-Saint-Michel is a tidal island and mainland commune in Normandy, France. The island lies approximately one kilometre off the country's north-western coast, at the mouth of the Couesnon River near Avranches and is 7 hectares in area.",
+        imageUrl: "img6.jpg"
     }
 ]
