3f5467718cf6ed07d70de08ff58d52aec0abd7fb
Dale <Danntabachoyy@gmail.com>
2023-02-23 18:01:51 +0800

    done

--- project2/src/components/TravelCard.jsx
+++ project2/src/components/TravelCard.jsx
@@ -12,7 +12,7 @@ const cards = dataTravel.map(items =>{
 console.log(dataTravel);
 export default function TravelCard(){
     return(
-        <div className="my-4 p-4 grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="container mx-auto my-4 p-4 grid grid-cols-1 md:grid-cols-2 gap-8">
             {cards}
         </div>
     ) 
