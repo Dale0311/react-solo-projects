6a9fdbe23b317d591132b1ef15e95d62bd1f317a
Dale <Danntabachoyy@gmail.com>
2023-02-28 21:50:24 +0800

    changes

--- project3/src/App.jsx
+++ project3/src/App.jsx
@@ -11,8 +11,20 @@ export default function App() {
       key = {box.id}
       id = {box.id}
       on = {box.on}
+      clickHandler = {clickHandler} 
     />)
   })
+
+  function clickHandler(id){
+    setboxes(prevSquare=>{
+      // think of this as variable = prevSqaure....
+      return prevSquare.map(square=>{
+        // think of this as returning the element to the variable
+                  // is the id matched? is so then change change my "on" key, if not matched then return the square
+        return square.id === id? {...square, on: !square.on}: square
+      })
+    })
+  }
 
   // to render
   return (
--- project3/src/components/Box.jsx
+++ project3/src/components/Box.jsx
@@ -1,14 +1,7 @@
-import { useState } from "react"
-
 export default function Box(props){
-    const [boxState, setBoxState] = useState(props.on);
-    const style = {backgroundColor: boxState? "#333": "orange"}
-    
-    // fn - handlers
-    function clickHandler(){
-        setBoxState(prevState => !prevState)
-    }
+
+    const style = {backgroundColor: props.on? "#333": "orange"}
     return(
-        <div style={style} className="p-14 rounded" key={props.id} onClick={clickHandler}></div>
+        <div style={style} className="p-14 rounded" key={props.id} onClick={()=>props.clickHandler(props.id)}></div>
     )
 }
