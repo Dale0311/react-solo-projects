42d013c8167f20cd20aa6b3ccbf798d3ca949456
Dale <Danntabachoyy@gmail.com>
2023-03-18 17:53:24 +0800

    restart

--- project3/src/App.jsx
+++ project3/src/App.jsx
@@ -1,36 +1,9 @@
 import { useState } from "react";
-import Box from "./components/Box"
-import boxData from "./data/boxes"
 export default function App() {
-
-  // setting custom component
-  const [boxes, setboxes] = useState(boxData);
-  const toRenderBox = boxes.map(box => {
-    return (
-    <Box 
-      key = {box.id}
-      id = {box.id}
-      on = {box.on}
-      clickHandler = {clickHandler} 
-    />)
-  })
-
-  function clickHandler(id){
-    setboxes(prevSquare=>{
-      // think of this as variable = prevSqaure....
-      return prevSquare.map(square=>{
-        // think of this as returning the element to the variable
-                  // is the id matched? is so then change change my "on" key, if not matched then return the square
-        return square.id === id? {...square, on: !square.on}: square
-      })
-    })
-  }
-
-  // to render
   return (
     <div className="container mx-auto">
-      <div className="my-10 grid grid-cols-3 gap-10 text-white text-center text-3xl">
-        {toRenderBox}
+      <div className="my-10 space-y-10 grid grid-cols-3 gap-10 text-white text-center text-3xl">
+        
       </div>
     </div>
   )
--- project3/src/components/Box.jsx
+++
@@ -1,7 +0,0 @@
-export default function Box(props){
-
-    const style = {backgroundColor: props.on? "#333": "orange"}
-    return(
-        <div style={style} className="p-14 rounded" key={props.id} onClick={()=>props.clickHandler(props.id)}></div>
-    )
-}
